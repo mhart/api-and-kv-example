73b254fe243f7a28a833f613eb5676ce782df539
Michael Hart <mhart@cloudflare.com>
2024-08-08 09:35:53 +1000

    Update to use latest C3 output

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,25 @@
-import { Hono } from "hono";
-type Bindings = { KV: KVNamespace };
-const app = new Hono<{ Bindings: Bindings }>();
+import { Hono } from 'hono';
 
-app.get("/repos/:username", async (c) => {
-  const username = c.req.param("username");
-  const cached = await c.env.KV.get(`repos:${username}`, "json");
+const app = new Hono<{ Bindings: Env }>();
 
-  if (cached) {
-    return c.json(cached);
-  } else {
-    const resp = await fetch(`https://api.github.com/users/${username}/repos`, {
-      headers: {
-        "User-Agent": "CF Workers",
-      },
-    });
-    const data = await resp.json();
-    await c.env.KV.put(`repos:${username}`, JSON.stringify(data), {
-      expirationTtl: 60,
-    });
-    return c.json(data);
-  }
+app.get('/repos/:username', async (c) => {
+	const username = c.req.param('username');
+	const cached = await c.env.KV.get(`repos:${username}`, 'json');
+
+	if (cached) {
+		return c.json(cached);
+	} else {
+		const resp = await fetch(`https://api.github.com/users/${username}/repos`, {
+			headers: {
+				'User-Agent': 'CF Workers',
+			},
+		});
+		const data = await resp.json();
+		await c.env.KV.put(`repos:${username}`, JSON.stringify(data), {
+			expirationTtl: 60,
+		});
+		return c.json(data as any);
+	}
 });
 
 export default app;
